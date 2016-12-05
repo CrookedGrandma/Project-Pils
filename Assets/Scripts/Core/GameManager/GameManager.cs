f0cf80bb1f9af709c464584ec087d0317e3cb096
Klaas-Jan Boon <awisoewas@gmail.com>
2016-12-05 11:50:27 +0100

    Adds singleton GameManager
    Add GameManager to a GameObject to make sure it's initialized in the
    scene

---
+++ Assets/Scripts/Core/GameManager/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public static MessageQueue messageQueue = new MessageQueue();
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void InitGame()
+    {
+
+    }
+
+}

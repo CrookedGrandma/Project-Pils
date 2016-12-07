ededdd0c6bf09961ccaf51f7a19b778ed2307deb
Steven Heinen <stevenheinen01@gmail.com>
2016-12-07 11:53:55 +0100

    input verbeterd

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -4,89 +4,134 @@ using System.Collections;
 public class Controls : MonoBehaviour
 {
 
-    public float Vel = 0.2f;
-    public float VelMultiplier = 100f;
-    public float JumpSpeed = 2f;
-    private bool IsPaused = false;
+    public float velocity = 20f;
+    public float sprintVelocity = 30f;
+    public float jumpSpeed = 3000f;
+    private float usedVelocity;
+    private bool isPaused = false;
+    private bool ableToJump, ableToMoveLeft, ableToMoveRight, ableToMoveForward, ableToMoveBackward;
     public Canvas canvas;
+    public Rigidbody PlayerRB;
+
+    private enum CollisionSides { Front, Back, Left, Right, Bottom, Top };
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         canvas.enabled = false;
+        PlayerRB = GetComponent<Rigidbody>();
     }
-	
-	
-	// Update is called once per frame
-	public void Update ()
+
+
+    // Update is called once per frame
+    public void Update()
     {
-        if (!IsPaused)
+        if (!isPaused)
         {
-            // Walking
-            float moveLeftRight = Input.GetAxis("Horizontal") * Vel * VelMultiplier * Time.deltaTime;
-            float moveForwardBackward = Input.GetAxis("Vertical") * Vel * VelMultiplier * Time.deltaTime;
+            // Determining wether we are walking or sprinting
+            if (Input.GetAxis("Sprint") <= 0)
+            {
+                // Walking
+                usedVelocity = velocity;
+            }
+            else
+            {
+                // Sprinting
+                usedVelocity = sprintVelocity;
+            }
+
+            // Walking or Sprinting
+            float moveLeftRight = Input.GetAxis("Horizontal") * usedVelocity * Time.deltaTime;
+            float moveForwardBackward = Input.GetAxis("Vertical") * usedVelocity * Time.deltaTime;
+            if (!ableToMoveLeft && moveLeftRight < 0)
+            {
+                moveLeftRight = 0;
+            }
+            if (!ableToMoveRight && moveLeftRight > 0)
+            {
+                moveLeftRight = 0;
+            }
+            if (!ableToMoveForward && moveForwardBackward > 0)
+            {
+                moveForwardBackward = 0;
+            }
+            if (!ableToMoveBackward && moveForwardBackward < 0)
+            {
+                moveForwardBackward = 0;
+            }
             transform.position += new Vector3(moveLeftRight, 0, moveForwardBackward);
 
-            // Jumping
-            /*
-             * This doesn't work as it is supposed to work. You can keep jumping endlessly by holding space.
-             * I've searched for a solution, but haven't found anything that works. 
-             * Possible solutions are using the "AddForce" method on the rigidbody, but then there is no movement, so it didn't work.
-             * It is still possible to just use the GetKey or GetKeyDown methods, but then you don't use the InputManager at all.
-            */
-            float jump = Input.GetAxis("Jump") * JumpSpeed * Time.deltaTime;
-            transform.position += new Vector3(0, jump, 0);
+            // Jump
+            if (Input.GetButtonDown("Jump") && ableToJump)
+            {
+                PlayerRB.AddForce(Vector3.up * jumpSpeed);
+                ableToJump = false;
+            }
 
-            // Normal walking
-            /*if (Input.GetKey(KeyCode.A))
-            {
-                transform.position += new Vector3(-Vel, 0, 0);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.position += new Vector3(Vel, 0, 0);
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                transform.position += new Vector3(0, 0, Vel);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.position += new Vector3(0, 0, -Vel);
-            }*/
-
-            // Jumping
-            /*if (Input.GetKeyDown(KeyCode.Space))
-            {
-                transform.position += new Vector3(0, JumpSpeed * Time.deltaTime, 0);
-            }*/
-
-            // Sprinting
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                Vel = 0.3f;
-            }
-            if (!Input.GetKey(KeyCode.LeftShift))
-            {
-                Vel = 0.2f;
-            }
+            //DEBUG
+            //Debug.Log("MovementSpeedLeftRight: " + moveLeftRight + "\nMovementSpeedForwardBackward: " + moveForwardBackward);
+            //END DEBUG
         }
 
         // Pause menu
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (IsPaused)
-            {    
+            if (isPaused)
+            {
                 Time.timeScale = 1;
-                IsPaused = false;
-                canvas.enabled = false;  
+                isPaused = false;
+                canvas.enabled = false;
             }
             else
             {
                 Time.timeScale = 0;
-                IsPaused = true;
+                isPaused = true;
                 canvas.enabled = true;
             }
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Determining if we can jump
+        //onGround = true;
+        ableToMoveBackward = ableToMoveForward = ableToMoveLeft = ableToMoveRight = true;
+
+        var normal = collision.contacts[0].normal;
+        if (normal.y > 0)
+        {
+            //Hit Bottom
+            Debug.Log("Hit bottom");
+            ableToJump = true;
+        }
+        /*else if (normal.y < 0)
+        {
+            //Hit Floor
+            Debug.Log("Hit roof");
+        }
+        else if (normal.x > 0)
+        {
+            //Hit Left
+            Debug.Log("Hit Left");
+            ableToMoveLeft = false;
+        }
+        else if (normal.x < 0)
+        {
+            //Hit Right
+            Debug.Log("Hit Right");
+            ableToMoveRight = false;
+        }
+        else if (normal.z < 0)
+        {
+            //Hit Front
+            Debug.Log("Hit Front");
+            ableToMoveForward = false;
+        }
+        else if (normal.z > 0)
+        {
+            //Hit Back
+            Debug.Log("Hit Back");
+            ableToMoveBackward = false;
+        }*/
     }
 }

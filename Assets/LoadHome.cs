52ba2d0b140da4fdcbe471d78095284af5afcd7f
Renzo Schindeler <renzo@schindeler.nl>
2016-12-16 12:27:44 +0100

    LoadHome script
    Laadt het Homelevel als je bij het onderste huis voor de deur staat en
    op E drukt.

---
+++ Assets/LoadHome.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadHome : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerStay(Collider trigger)
+    {
+        print("Home Scene trigger active");
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Application.LoadLevel("Home");
+            print("Loading Home Scene");
+        }
+
+    }
+
+}

cdc7fe2c26abd2111981ba0a12ec81a648939579
Renzzauw <Renzo@dccomputers.local>
2017-01-15 14:54:26 +0100

    Lens Effecten || Camera Zoom Script (werkt beperkt)

---
+++ Assets/CamMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    public GameObject Parent;
+
+    //Checks if the camera collides with something
+    void OnTriggerStay(Collider other)
+    {
+        //If true, the camera moves up and back from the player object          
+        transform.position += new Vector3(0, 0.2f, -0.2f);
+    }
+
+
+    void Update()
+    {
+        //makes sure the camera always looks at the player object   
+        transform.LookAt(Parent.transform);
+        /* if (transform.localPosition.y > 5.04f)
+         {
+             transform.localPosition = new Vector3(0, -0.05f, +0.05f);
+         } */
+    }
+    /*
+    void OnTriggerExit(Collider other)
+    {
+        transform.localPosition = new Vector3(0f, 5.04f, -11.3f);
+    }*/
+}

f9d25934b52edd5398a00b1c5668c5e7c2c1a675
CrookedGrandma <k.t.noot@live.nl>
2017-01-15 17:36:05 +0100

    Begin implementatie abilities en commentwijziging voor inventory

---
+++ Assets/Scripts/Combat/Ability.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability {
+
+    public int ID { get; set; }
+    public string Type { get; set; }
+    public string Title { get; set; }
+    public int WeaponID { get; set; }
+
+    //Constructor for an ability
+    /// <summary>
+    /// Create a new ability
+    /// </summary>
+    public Ability(int id, string type, string title, int weaponid) {
+        this.ID = id;
+        this.Type = type;
+        this.Title = title;
+        this.WeaponID = weaponid;
+    }
+}
---
+++ Assets/Scripts/Combat/AbilityChooser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using LitJson;
+
+public class AbilityChooser : MonoBehaviour {
+
+    public List<Ability> UsableAbilities = new List<Ability>();
+    public List<Ability> AbilityDatabase = new List<Ability>();
+    public JsonData Abilities;
+
+	// Use this for initialization
+	void Start () {
+        Abilities = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Abilities.json"));
+        ConstructAbilityDatabase();
+        FindUsables();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void ConstructAbilityDatabase() {
+        for (int i = 0; i < Abilities.Count; i++) {
+            AbilityDatabase.Add(new Ability((int)Abilities[i]["id"], Abilities[i]["type"].ToString(),
+                                            Abilities[i]["title"].ToString(), (int)Abilities[i]["weaponid"]));
+        }
+    }
+
+    private void FindUsables() {
+        for (int i = 0; i < AbilityDatabase.Count; i++) {
+
+        }
+    }
+}

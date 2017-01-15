a5c4e2c2f166179ab19e4ec480e490a1824d2da9
Stevieboy-AC <stevenheinen01@gmail.com>
2017-01-15 11:37:55 +0100

    LoadHome heet nu LoadLevel, level laden op sommige plekken toegevoegd
    Geef de naam van het te laden level op als string bij de collider om dat
    level te laden

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string levelName;
+
+    /// <summary>
+    /// Checks's if the player is in the Collider area (trigger).
+    /// When triggered, if the "Accept"-button is pressed, load the Home scene.
+    public void OnTriggerStay(Collider trigger)
+    {
+        print(levelName + " Scene trigger active");
+        if (Input.GetButtonDown("Accept"))
+        {
+            Application.LoadLevel(levelName);
+            print("Loading " + levelName + " Scene");
+        }
+    }
+}

06b83448dde811aaeaf349cca9dd9b0e09b7912a
CrookedGrandma <k.t.noot@live.nl>
2017-01-15 18:24:42 +0100

    Verdere implementatie abilities

--- Assets/Scripts/Combat/AbilityChooser.cs
+++ Assets/Scripts/Combat/AbilityChooser.cs
@@ -10,16 +10,23 @@ public class AbilityChooser : MonoBehaviour {
     public List<Ability> AbilityDatabase = new List<Ability>();
     public JsonData Abilities;
 
-	// Use this for initialization
-	void Start () {
+    string weapon1;
+    string weapon2;
+
+    public static string Ability1Text;
+    public static string Ability2Text;
+    public static int selectedAbility;
+
+    // Use this for initialization
+    void Start () {
         Abilities = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Abilities.json"));
         ConstructAbilityDatabase();
         FindUsables();
+        weapon1 = PersistentInventoryScript.instance.itemList[UsableAbilities[0].WeaponID, 0].ToString();
+        weapon2 = PersistentInventoryScript.instance.itemList[UsableAbilities[1].WeaponID, 0].ToString();
+        print("Weapon 1 is " + weapon1);
+        FillText();
     }
-	
-	// Update is called once per frame
-	void Update () {
-	}
 
     private void ConstructAbilityDatabase() {
         for (int i = 0; i < Abilities.Count; i++) {
@@ -29,8 +36,31 @@ public class AbilityChooser : MonoBehaviour {
     }
 
     private void FindUsables() {
+        int equippedWeapon = PersistentInventoryScript.instance.equipmentList[0, 0];
+        int equippedRanged = PersistentInventoryScript.instance.equipmentList[1, 0];
         for (int i = 0; i < AbilityDatabase.Count; i++) {
+            if (AbilityDatabase[i].WeaponID == equippedWeapon) {
+                UsableAbilities[0] = AbilityDatabase[i];
+            }
+            if (AbilityDatabase[i].WeaponID == equippedRanged) {
+                UsableAbilities[1] = AbilityDatabase[i];
+            }
+        }
+    }
 
+    private void FillText() {
+        Ability1Text = UsableAbilities[0].Title + " <color=#bbbbbb>using your</color> <color=#cccccc>" + weapon1 + "</color>";
+        Ability2Text = UsableAbilities[1].Title + " <color=#bbbbbb>using your</color> <color=#cccccc>" + weapon2 + "</color>";
+    }
+
+    public void SelectAbility(int a) {
+        if (a == 1) {
+            Ability1Text = "<color=red>" + UsableAbilities[0].Title + " <color=#ffbbbb>using your</color> <color=#ffcccc>" + weapon1 + "</color>";
+            selectedAbility = 1;
+        }
+        if (a == 2) {
+            Ability2Text = "<color=red>" + UsableAbilities[1].Title + " <color=#ffbbbb>using your</color> <color=#ffcccc>" + weapon2 + "</color>";
+            selectedAbility = 2;
         }
     }
 }

ce6ab49eb787cbac86aecc03098281f68cdc53a5
Stevieboy-AC <stevenheinen01@gmail.com>
2017-01-15 14:14:33 +0100

    Revert "LoadHome heet nu LoadLevel, level laden op sommige plekken toegevoegd"
    This reverts commit a5c4e2c2f166179ab19e4ec480e490a1824d2da9.
    
    # Conflicts:
    #	Assets/LoadLevel.cs
    #	Assets/LoadLevelCollider.prefab

---
+++ Assets/LoadHome.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadHome : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Checks's if the player is in the Collider area (trigger).
+    /// When triggered, if the "E"-button is pressed, load the Home scene.
+    public void OnTriggerStay(Collider trigger)
+    {
+        print("Home Scene trigger active");
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Application.LoadLevel("Home");
+            print("Loading Home Scene");
+        }
+
+    }
+
+}

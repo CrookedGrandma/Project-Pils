1ed89deb17536c52b3364f7e8d2acdf2bb32bda7
Stevieboy-AC <stevenheinen01@gmail.com>
2017-01-15 12:57:28 +0100

    Revert "Eerste versie PlayerPrefsManager"
    This reverts commit 953e62c88fd2a3d0e5f308d604099b57522aae07.

--- Assets/LoadLevel.cs
+++ Assets/LoadLevel.cs
@@ -4,12 +4,6 @@ using System.Collections;
 public class LoadLevel : MonoBehaviour
 {
     public string levelName;
-    public GameObject player;
-
-    private void Awake()
-    {
-        player = GameObject.Find("Player");
-    }
 
     /// <summary>
     /// Checks's if the player is in the Collider area (trigger).
@@ -19,10 +13,7 @@ public class LoadLevel : MonoBehaviour
         print(levelName + " Scene trigger active");
         if (Input.GetButtonDown("Accept"))
         {
-            PlayerPrefsManager.SetPositionInLevel(Application.loadedLevelName, player);
             Application.LoadLevel(levelName);
-            Vector3 newPosition = PlayerPrefsManager.GetPositionInLevel(levelName, player);
-            player.transform.position = newPosition;
             print("Loading " + levelName + " Scene");
         }
     }

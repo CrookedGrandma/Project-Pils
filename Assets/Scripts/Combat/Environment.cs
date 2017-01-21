0fa3a5e16fa75edd1968dda925ee6307fb2df18c
CrookedGrandma <k.t.noot@live.nl>
2017-01-21 15:05:14 +0100

    Environment doet het weer

--- Assets/Scripts/Combat/Environment.cs
+++ Assets/Scripts/Combat/Environment.cs
@@ -1,30 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Environment : MonoBehaviour
 {
-
-    public Camera MainCamera;
+    
+    public Image BG;
 
     public Color Desert = Color.black;
     public Color Sea = Color.black;
     public Color Forest = Color.black;
     public Color Sky = Color.black;
-    //Set these colors in Unity Editor, replace with backdrop sprites later on
-
-    void Start() {
-        //MainCamera = FindObjectOfType<Camera>();
-    }
+    //Set these colors in Unity Editor (EnemyChooser), replace with backdrop sprites later on
 
     public void changebg(int bgcolor)
     {
         switch (bgcolor)
         {
-            case 0: MainCamera.backgroundColor = Desert; break;
-            case 1: MainCamera.backgroundColor = Sea; break;
-            case 2: MainCamera.backgroundColor = Forest; break;
-            case 3: MainCamera.backgroundColor = Sky; break;
-            default: MainCamera.backgroundColor = Color.white; break;
+            case 0: BG.color = Desert; break;
+            case 1: BG.color = Sea; break;
+            case 2: BG.color = Forest; break;
+            case 3: BG.color = Sky; break;
+            default: BG.color = Color.white; break;
         }
     }
 }

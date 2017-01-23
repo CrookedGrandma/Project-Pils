ac3f962a0826f954b27139e2aff0681f0b5bd8af
Stevieboy-AC <stevenheinen01@gmail.com>
2017-01-23 17:04:03 +0100

    music 3.0

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -8,10 +8,19 @@ public class MusicManager : MonoBehaviour
     public AudioClip[] levelMusicChangeArray;
 
     private AudioSource audioSource;
+    private static MusicManager instance;
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     private void Start()
@@ -25,7 +34,22 @@ public class MusicManager : MonoBehaviour
         {
             AudioClip thisLevelMusic = levelMusicChangeArray[level];
 
-            if (level > 2 && level < 18)
+            if (level < 2)
+            {
+                // If the current playing clip is the clip that has to be played at the first real scene in the build settings (Home)
+                if (audioSource.clip == levelMusicChangeArray[0])
+                {
+                    // Do nothing
+                }
+                else
+                {
+                    audioSource.volume = PlayerPrefsManager.GetMusicVolume();
+                    audioSource.clip = levelMusicChangeArray[0];
+                    audioSource.loop = true;
+                    audioSource.Play();
+                }
+            }
+            else if (level > 2 && level < 18)
             {
                 // Normal ingame music has to be played here
 

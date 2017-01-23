f071f5d8b5a573ac7b3b7b89d8620b5ca2a3b75d
Stevieboy-AC <stevenheinen01@gmail.com>
2017-01-23 20:33:27 +0100

    FPS cap in main menu (consistentere loopsnelheden)

---
+++ Assets/Scripts/FPS_Cap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Cap : MonoBehaviour
+{
+    public int maxFPS = 60;
+
+    private static FPS_Cap instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        Application.targetFrameRate = maxFPS;
+    }
+}

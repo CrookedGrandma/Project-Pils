ace4d5b9a5d49411a2b3c185ed932cb40d1839f3
Stevieboy-AC <stevenheinen01@gmail.com>
2017-01-24 19:01:06 +0100

    begin aan naar combat in dungeon

--- Assets/Scripts/Dungeons/Dungeon_Enemy.cs
+++ Assets/Scripts/Dungeons/Dungeon_Enemy.cs
@@ -7,10 +7,12 @@ public class Dungeon_Enemy : MonoBehaviour
 {
     private bool canBeFought = true;
     private GameObject player;
+    private PlayerFSM playerFSM;
 
     private void Start()
     {
         player = GameObject.Find("Player");
+        playerFSM = GameObject.FindObjectOfType<PlayerFSM>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,15 +20,13 @@ public class Dungeon_Enemy : MonoBehaviour
         if (other.name == "Player" && canBeFought)
         {
             // Enter combat scene
+            canBeFought = false;
             PlayerPrefsManager.SetCurrentScene(SceneManager.GetActiveScene().name);
             PlayerPrefsManager.SetPositionInLevel(SceneManager.GetActiveScene().name, player);
             SceneManager.LoadScene("Combat");
             player.transform.position = PlayerPrefsManager.GetPositionInLevel("Combat", player);
-
-            // DEBUG
-            SceneManager.LoadScene(PlayerPrefsManager.GetCurrentScene());
-            player.transform.position = PlayerPrefsManager.GetPositionInLevel(PlayerPrefsManager.GetCurrentScene(), player);
-            // END DEBUG
+            playerFSM.Enemy = 4;
+            playerFSM.Envi = 0;
         }
     }
 }

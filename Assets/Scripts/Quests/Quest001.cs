9d0f42cd0c74a9b39fc0d628823a32e77978d3be
Klaas-Jan Boon <awisoewas@gmail.com>
2017-01-24 21:51:44 +0100

    quest hotfix

--- Assets/Scripts/Quests/Quest001.cs
+++ Assets/Scripts/Quests/Quest001.cs
@@ -7,11 +7,6 @@ public class Quest001 : MonoBehaviour {
 	void OnTriggerEnter()
     {
         GameManager.instance.questManager.CompleteObjective("Quest001FindPhone");
-
-        Message m = new Message(GameManager.instance.questManager, GameManager.instance.dialogueManager, MsgType.Dialogue, "You found your phone in the closet!");
-        GameManager.instance.messageQueue.Add(m);
-
-
         GameManager.instance.questManager.AddQuestToLog("Quest002");
     }
 }

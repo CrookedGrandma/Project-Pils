70070e336d909cf2edeeb4f1680ea24fd020c1c6
Hidde Veer <hidde.veer@gmail.com>
2017-01-30 14:33:34 +0100

    Klik voor items in inventory weggehaalt

--- Assets/Scripts/Inventory/ItemData.cs
+++ Assets/Scripts/Inventory/ItemData.cs
@@ -131,63 +131,7 @@ public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     }
         else if (slot < inv.slotCount)
         {
-            if (item.Type == "equipment" || item.Type == "weapon")
-            {
-                for (int x = 0; x < amount; x++)
-                {
-                    persistentInventory.removeItem(item.ID, slot);
-                }
-                offset = eventData.position - new Vector2(this.transform.position.x, this.transform.position.y);
-                this.transform.SetParent(this.transform.parent.parent);
-                this.transform.position = eventData.position - offset;
-                GetComponent<CanvasGroup>().blocksRaycasts = false;
-                if (item.Subtype == "melee")
-                {
-                    this.transform.SetParent(inv.slots[inv.slotCount].transform);
-                    this.transform.position = inv.slots[inv.slotCount].transform.position;
-                    slot = inv.slotCount;
-                    persistentInventory.addEquipment(item.ID, amount, 0);
-                }
-                if (item.Subtype == "ranged" || item.Subtype == "projectile")
-                {
-                    this.transform.SetParent(inv.slots[inv.slotCount + 1].transform);
-                    this.transform.position = inv.slots[inv.slotCount + 1].transform.position;
-                    slot = inv.slotCount + 1;
-                    {
-                        persistentInventory.addEquipment(item.ID, amount, 1);
-                    }
-                }
-                if (item.Subtype == "headwear")
-                {
-                    this.transform.SetParent(inv.slots[inv.slotCount + 2].transform);
-                    this.transform.position = inv.slots[inv.slotCount + 2].transform.position;
-                    slot = inv.slotCount + 2;
-                    persistentInventory.addEquipment(item.ID, amount, 2);
-                }
-                if (item.Subtype == "bodywear")
-                {
-                    this.transform.SetParent(inv.slots[inv.slotCount + 3].transform);
-                    this.transform.position = inv.slots[inv.slotCount + 3].transform.position;
-                    slot = inv.slotCount + 3;
-                    persistentInventory.addEquipment(item.ID, amount, 3);
-                }
-                if (item.Subtype == "lowerwear")
-                {
-                    this.transform.SetParent(inv.slots[inv.slotCount + 4].transform);
-                    this.transform.position = inv.slots[inv.slotCount + 4].transform.position;
-                    slot = inv.slotCount + 4;
-                    persistentInventory.addEquipment(item.ID, amount, 4);
-                }
-                if (item.Subtype == "footwear")
-                {
-                    this.transform.SetParent(inv.slots[inv.slotCount + 5].transform);
-                    this.transform.position = inv.slots[inv.slotCount + 5].transform.position;
-                    slot = inv.slotCount + 5;
-                    persistentInventory.addEquipment(item.ID, amount, 5);
-                }
-                GetComponent<CanvasGroup>().blocksRaycasts = true;
-            }
-            else if(item.Type == "consumable")
+            if(item.Type == "consumable")
             {
                 switch (item.ID)
                 {
